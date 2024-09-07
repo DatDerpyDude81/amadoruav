ec43d4e9bfe0b701911cfe6ae00c3bac84842f94
DatDerpyDude81 <myanghpms2021@gmail.com>
2024-09-06 20:24:30 -0700

    trying to get outlier removal working

--- pt0.py
+++ pt0.py
@@ -1,9 +1,17 @@
-import numpy
+#imporrrtttsssss
+
 from sklearn.cluster import KMeans
+from sklearn.neighbors import LocalOutlierFactor
 import numpy as np
-
+#load data as a numpy array
 data=np.loadtxt("inferences.txt",dtype=np.float64)
+#create sklearn outlier factor object
+outliers=LocalOutlierFactor(n_neighbors=25).fit_predict(data)
+for line in data.readline():
+    if outliers[line]==-1:
+        data.pop(line)
 
+#create KMeans clustering model based on data
 kmeans = KMeans(n_clusters=5,random_state=0, n_init="auto").fit(data)
 
 print(kmeans.cluster_centers_) 
