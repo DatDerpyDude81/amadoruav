aa40e29a8b55fccab1fd975682985521f8fa26c0
DatDerpyDude81 <myanghpms2021@gmail.com>
2024-09-06 19:59:19 -0700

    yeahhh screw geopandas,trying sklearn

--- pt0.py
+++ pt0.py
@@ -1,10 +1,5 @@
-import geopandas
-import re
-input=open("inferences.txt","r")
-WKT=[]
-for line in input:
-    line = re.sub(r'[\x00-\x1f]', '', line)
-    WKT.append(f"Point ("+line+")")
+import numpy
+import sklearn
+import numpy as np
 
-geopandaData=geopandas.GeoSeries.from_wkt(WKT, index=None, crs=None, on_invalid='raise')
-print(geopandaData)
+data=np.loadtxt("inferences.txt",dtype=np.float64)
