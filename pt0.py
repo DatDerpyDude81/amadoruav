fa7c307b1e763c7cd30487e80bc146b903dc1810
DatDerpyDude81 <myanghpms2021@gmail.com>
2024-09-06 20:09:58 -0700

    W sklearn

--- pt0.py
+++ pt0.py
@@ -1,5 +1,9 @@
 import numpy
-import sklearn
+from sklearn.cluster import KMeans
 import numpy as np
 
 data=np.loadtxt("inferences.txt",dtype=np.float64)
+
+kmeans = KMeans(n_clusters=5,random_state=0, n_init="auto").fit(data)
+
+print(kmeans.cluster_centers_) 
