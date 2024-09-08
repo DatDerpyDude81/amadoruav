42926c14c97911d55bf85ee5530111fcf5a116db
DatDerpyDude81 <myanghpms2021@gmail.com>
2024-09-08 10:40:59 -0700

    delete waypoints that are outside of the geofence

--- pt1/pt1.py
+++ pt1/pt1.py
@@ -13,16 +13,36 @@ geofencecoords=data[1:geofenceindex]
 waypointcoords=data[geofenceindex:waypointindex+geofenceindex]
 
 
+
 geofence=shapely.Polygon(geofencecoords)
+
+
+#redmvoe waypoints that are outside of the geofence
+index=0
+for x,y in waypointcoords:
+    
+    if geofence.contains(shapely.Point(x,y))==False:
+        waypointcoords=np.delete(waypointcoords,index,0)
+    index+=1
+
+
+
+
+
+
 waypoint=shapely.Polygon(waypointcoords)
+
+intersect=shapely.intersection(geofence,waypoint)
+
+x,y=intersect.exterior.xy
+plt.plot(x,y,"r")
 
 gx,gy = geofence.exterior.xy
 wx,wy= waypoint.exterior.xy
-
-
-for x,y in np.nditer((wx,wy)):
-    
-
+plt.plot(gx,gy,"b")
+gx,gy = geofence.exterior.xy
+wx,wy= waypoint.exterior.xy
 plt.plot(gx,gy)
 plt.plot(wx,wy)
+
 plt.show()
