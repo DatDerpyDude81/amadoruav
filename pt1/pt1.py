cc7ceebf78c906227e3cecc0fb4f99ce30755914
DatDerpyDude81 <myanghpms2021@gmail.com>
2024-09-08 17:52:54 -0700

    p a i n

--- pt1/pt1.py
+++ pt1/pt1.py
@@ -17,7 +17,8 @@ waypointcoords=data[geofenceindex:waypointindex+geofenceindex]
 geofence=shapely.Polygon(geofencecoords)
 
 
-#redmvoe waypoints that are outside of the geofence
+#remove waypoints that are outside of the geofence
+#why do I even have to do this
 index=0
 for x,y in waypointcoords:
     
@@ -34,15 +35,25 @@ waypoint=shapely.Polygon(waypointcoords)
 
 intersect=shapely.intersection(geofence,waypoint)
 
-x,y=intersect.exterior.xy
-plt.plot(x,y,"r")
 
-gx,gy = geofence.exterior.xy
-wx,wy= waypoint.exterior.xy
-plt.plot(gx,gy,"b")
-gx,gy = geofence.exterior.xy
-wx,wy= waypoint.exterior.xy
+#If a vertice on the intersected polygon is on the geofence, then it will have 2 neighbors that are also on the geofence
+#so we detect the "middle" vertice, keep it ,a delete the other 2 vertices, forming a smooth path around the geofence
+coords=intersect.exterior.coords
+ind=0
+for coord in coords:
+    if shapely.touches(geofence,shapely.Point(coord)):
+        pass
+        
+    ind+=1
+
+
+gx,gy=geofence.exterior.xy
 plt.plot(gx,gy)
-plt.plot(wx,wy)
+ix,iy=intersect.exterior.xy
+plt.plot(ix,iy,"bo")
+
+
+
+
 
 plt.show()
