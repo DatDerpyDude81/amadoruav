080bd546734f823e2d82ec369fb3d9df65d61928
DatDerpyDude81 <myanghpms2021@gmail.com>
2024-09-27 10:03:06 -0700

    shapessss

--- pt2/pt2.py
+++ pt2/pt2.py
@@ -1,19 +1,41 @@
 from PIL import Image, ImageDraw,ImageFont
+import PIL
 import random
+import math
 import ultralytics
 alphabet=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-dim=random.randint(100,1000)
-shapes=["circle","semicircle" "quarter circle","triangle","rectangle","pentagon","star","cross"]
+
+shapes=["circle","semicircle","quartercircle","triangle","rectangle","pentagon","star","cross"]
 for shape in shapes:
     for letter in alphabet:
-        posx=random.randint(10,dim-20)
-        posy=random.randint(10,dim-20)
-        size=random.randint(15,50)
+        dim=random.randint(100,1000)
+        size=random.randint(15,math.floor(dim*0.5))
+        posx=random.randint(10,math.floor(dim*0.9))
+        posy=random.randint(10,math.floor(dim*0.9))
+        if posx<size:
+            posx=size
+        if size+posx>dim:
+            posx=dim-size
+        if posy<size:
+            posy=size
+        if size+posy>dim:
+            posy=dim-size
+    
         im = Image.new(mode="RGB", size=(dim, dim))
+
         draw = ImageDraw.Draw(im)
         font = ImageFont.truetype("pt2/sans-serif.ttf", size)
-        draw.text((posx, posy),letter,(255,255,255),font=font)
-        im=im.rotate(random.randint(0,360))
+        if shape=="circle":
+            draw.circle((posx,posy), size, fill=(random.randint(0,255),random.randint(0,255),random.randint(0,255)), outline=None, width=1)
+        if shape=="semicircle":
+            draw.pieslice([(posx-size,posy-size*1.5),(posx+size,posy+size*0.5)],0,180,fill=(random.randint(0,255),random.randint(0,255),random.randint(0,255)))
+        if shape=="quartercircle":
+            draw.pieslice([(posx-size*1.3,posy-size*1.5),(posx+size*0.7,posy+size*0.5)],0,90,fill=(random.randint(0,255),random.randint(0,255),random.randint(0,255)))
+            
+        
+
+        draw.text((posx, posy),letter,(255,255,255),font=font,anchor="mm")
+
         im.save(f"{letter}{shape}.jpg")
 
         
