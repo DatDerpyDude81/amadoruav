ec0f393fbf4fed3eae605bffeccfb11d77e1ff6a
DatDerpyDude81 <myanghpms2021@gmail.com>
2024-09-28 23:35:32 -0700

    more training

--- pt2/train.py
+++ pt2/train.py
@@ -1,3 +1,3 @@
 from ultralytics import YOLO
 model = YOLO("yolov8n-cls.yaml",task="classify")
-reuslts = model.train(data="/Users/matthewyang/amadoruav/pt2/data", model="yolov8n-cls.yaml",epochs=10)
+reuslts = model.train(data="/Users/matthewyang/amadoruav/pt2/data", model="yolov8n-cls.yaml",epochs=50)
